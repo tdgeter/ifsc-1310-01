c7de83daf92982aeba4529d8599f470374e2824b
tdgeter <112530212+tdgeter@users.noreply.github.com>
2024-12-08 07:30:41 +0000

    Finish getting my javascrript implemented into the final project, add a contact form page.

--- final-v2/assets/js/scripts.js
+++ final-v2/assets/js/scripts.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function() {
 
-    $('.fade').slick({
+    $('.slick').slick({
       dots: true,
       infinite: true,
       speed: 1000,
       autoplay: true,
       autoplaySpeed: 2750,
-      adaptiveHeight: true
+      adaptiveHeight: true,
     });
 
 
